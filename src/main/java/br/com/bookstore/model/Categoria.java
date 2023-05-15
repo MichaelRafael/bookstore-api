8b380a44aeaa6f8bfdff7437075f1c6b6d498b07
= <=>
2023-05-15 11:48:58 -0300

    criação das models

---
+++ src/main/java/br/com/bookstore/model/Categoria.java
@@ -0,0 +1,71 @@
+package br.com.bookstore.model;
+
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class Categoria {
+
+    private Integer id;
+    private String nome;
+    private String descricao;
+
+    @OneToMany
+    private List<Livro> livro = new ArrayList<>();
+
+    public Categoria() {
+        super();
+    }
+
+    public Categoria(Integer id, String nome, String descricao) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public List<Livro> getLivro() {
+        return livro;
+    }
+
+    public void setLivro(List<Livro> livro) {
+        this.livro = livro;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Categoria)) return false;
+        Categoria categoria = (Categoria) o;
+        return id.equals(categoria.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
