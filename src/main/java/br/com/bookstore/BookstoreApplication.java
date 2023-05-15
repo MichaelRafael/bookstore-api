c4ebea567d80b2d9097d8c9dce07299cc790b45c
= <=>
2023-05-15 14:36:57 -0300

    perfi de test implementado

--- src/main/java/br/com/bookstore/BookstoreApplication.java
+++ src/main/java/br/com/bookstore/BookstoreApplication.java
@@ -13,28 +13,10 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 @SpringBootApplication
-public class BookstoreApplication implements CommandLineRunner {
-
-	@Autowired
-	private CategoriaRepository categoriaRepository;
-
-	@Autowired
-	private LivroRepository livroRepository;
+public class BookstoreApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-
-		Categoria cat1 = new Categoria(null, "Informática", "Livro de TI");
-
-		Livro l1 = new Livro(null, "Clean code", "Robertin Martin", "Lorem ipsum", cat1);
-
-		cat1.getLivro().addAll(Arrays.asList(l1));
-
-		this.categoriaRepository.saveAll(Arrays.asList(cat1));
-		livroRepository.saveAll(Arrays.asList(l1));
-	}
 }
---
+++ src/main/java/br/com/bookstore/service/DBService.java
@@ -0,0 +1,38 @@
+package br.com.bookstore.service;
+
+import br.com.bookstore.model.Categoria;
+import br.com.bookstore.model.Livro;
+import br.com.bookstore.repository.CategoriaRepository;
+import br.com.bookstore.repository.LivroRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+
+@Service
+public class DBService {
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
+    @Autowired
+    private LivroRepository livroRepository;
+
+
+    public void instaciaDB() {
+
+        Categoria cat1 = new Categoria(null, "Informática", "Livro de TI");
+        Categoria cat2 = new Categoria(null, "Ficção Científica", "Ficção Científica");
+        Categoria cat3 = new Categoria(null, "Biografias", "Livros de Biografias");
+
+        Livro l1 = new Livro(null, "Clean code", "Robertin Martin", "Lorem ipsum", cat1);
+        Livro l2 = new Livro(null, "Engenharia de Software", "Louis V. Gerstner", "Lorem ipsum", cat1);
+        Livro l3 = new Livro(null, "The time machine", "H. G. Wells", "Lorem ipsum", cat2);
+        Livro l4 = new Livro(null, "The war of the worlds", "H. G. Wells", "Lorem ipsum", cat2);
+        Livro l5 = new Livro(null, "I, robot", "Isaac Asimov", "Lorem ipsum", cat2);
+
+        cat1.getLivro().addAll(Arrays.asList(l1));
+
+        this.categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+        livroRepository.saveAll(Arrays.asList(l1, l2, l3, l4, l5));
+    }
+}
